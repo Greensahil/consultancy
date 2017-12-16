40f059aa63897b38936b11a19cbd9548c4a12691
Sahil Sharma <sahilwhiteday@gmail.com>
2017-12-16 23:30:40 +0000

    added register routes and forms

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express= require('express'),
-     app=   express(),
+    app=   express(),
+    passport=require('passport'),
+    LocalStrategy=require('passport-local'),
     mongoose=require("mongoose"),
-    bodyParser= require('body-parser');
+    bodyParser= require('body-parser'),
+    User        = require("./models/user");
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine','ejs');
@@ -20,6 +23,20 @@ var blogSchema= new mongoose.Schema({
     created:{type:Date,default:Date.now}
 });
 var Blog=mongoose.model("Blog",blogSchema);
+
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Subha Mishra",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 
@@ -84,6 +101,53 @@ app.get("/blogs/:id", function(req, res){
         }
     });
 })
+
+//  ===========
+// AUTH ROUTES
+//  ===========
+
+// show register form
+app.get("/register", function(req, res){
+   res.render("register"); 
+});
+//handle sign up logic
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+           res.redirect("/campgrounds"); 
+        });
+    });
+});
+
+// show login form
+app.get("/login", function(req, res){
+   res.render("login"); 
+});
+// handling login logic
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }), function(req, res){
+});
+
+// logic route
+app.get("/logout", function(req, res){
+   req.logout();
+   res.redirect("/blogs");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
