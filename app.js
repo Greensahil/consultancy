13911f94ec298f4250e19df34134e72ad115b938
Sahil Sharma <sahilwhiteday@gmail.com>
2017-12-23 23:44:48 +0000

    added static files for css

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var express         =   require('express'),
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine','ejs');
+app.use(express.static(__dirname + "/public"));
 seedDB();
 
 var url = process.env.DATABASEURL || "mongodb://localhost/consultancy";
