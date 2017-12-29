cd2f968be1686143ce9ddd97aa28a705facbf6c2
Sahil Sharma <sahilwhiteday@gmail.com>
2017-12-29 00:30:17 +0000

    add authorization for updating and deleting blogs

--- routes/blogs.js
+++ routes/blogs.js
@@ -75,31 +75,10 @@ router.get("/:id",function(req, res){
 
 
 //EDIT blog route
-router.get("/:id/edit",function(req,res){
-    //Checking is the user is logged in
-     if (req.isAuthenticated()){
-        Blog.findById(req.params.id,function(err,foundBlog){
-        if(err){
-            res.redirect("/blogs");
-        }
-        else{
-            //Checking if the user owns the blog
-            if(foundBlog.author.id.equals(req.user._id)){
-                res.render("blogs/edit",{blog:foundBlog});
-            }
-            else{
-                 res.send("You do not have permission to do that")
-            }
-            
-        }
+router.get("/:id/edit",checkBlogOwnership,function(req,res){
+    Blog.findById(req.params.id,function(err,foundBlog){
          res.render("blogs/edit",{blog:foundBlog});
-    });
-         
-     }
-
-    
-    
-
+});
 });
 
 
@@ -109,7 +88,7 @@ router.get("/:id/edit",function(req,res){
 
 //UPDATE blog route
 
-router.put("/:id",function(req,res){
+router.put("/:id",checkBlogOwnership,function(req,res){
     Blog.findByIdAndUpdate(req.params.id,req.body.blog,function(err,updatedBlog){
         if(err){
             res.redirect("/blogs");
@@ -124,7 +103,7 @@ router.put("/:id",function(req,res){
 
 
 //DESTROY route
-router.delete(":/id",function(req,res){
+router.delete("/:id",checkBlogOwnership,function(req,res){
     Blog.findByIdAndRemove(req.params.id,function(err){
         if(err){
             res.redirect("/blogs");
@@ -144,4 +123,41 @@ function isLoggedIn(req, res, next){
     }
     res.redirect("/login");
 }
+
+
+function checkBlogOwnership(req,res,next){
+     if (req.isAuthenticated()){
+        Blog.findById(req.params.id,function(err,foundBlog){
+        if(err){
+            //This will redirect back to show page
+            res.redirect("back");
+        }
+        else{
+            //Checking if the user owns the blog
+            if(foundBlog.author.id.equals(req.user._id)){
+                next();
+            }
+            else{
+                 res.redirect("back")
+            }
+            
+        }
+         
+    });
+         
+     }else{
+         
+         res.redirect("back")
+         
+         
+     }
+            
+            
+
+}
+
+
+
+
+
 module.exports= router;
